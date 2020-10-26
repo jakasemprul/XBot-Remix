03a3e14884b31400b5f78952370a362c38c017ae
zankee <66905046+zankee@users.noreply.github.com>
2020-10-26 22:41:56 +0700

    Update bayar.py

--- userbot/modules/bayar.py
+++ userbot/modules/bayar.py
@@ -6,5 +6,5 @@ from userbot.events import register
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(3)
-    await typew.edit("DANA 085877240759 PULSA 082114264663")
+    await typew.edit("📱DANA 085877240759 📱PULSA 082114264663")
 # Create by myself @localheart
