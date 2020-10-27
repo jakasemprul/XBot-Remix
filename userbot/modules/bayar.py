06062d4e4b03c98b2908e024554153df48224ec7
X-Newbie <sxnewbie@gmail.com>
2020-10-27 10:11:51 +0000

    pylint: auto fixes
    Signed-off-by: X-Newbie <sxnewbie@gmail.com>

--- userbot/modules/bayar.py
+++ userbot/modules/bayar.py
@@ -1,5 +1,5 @@
 from time import sleep
-from userbot import CMD_HELP, bot
+from userbot import CMD_HELP
 from userbot.events import register
 
 
@@ -10,13 +10,13 @@ async def typewriter(typew):
     await typew.edit("📱DANA 085877240759 📱PULSA 082114264663")
 # Create by myself @localheart
 
+
 @register(outgoing=True, pattern='^.list(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(3)
     await typew.edit("SSH INDO 🇮🇩 10 K   SSH SG 🇨🇱 15 K")
 # Create by myself @localheart
-
 
 
 CMD_HELP.update({
