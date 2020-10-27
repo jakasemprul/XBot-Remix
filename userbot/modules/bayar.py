ab442f754a207729ec6427414183c8078318060c
zankee <66905046+zankee@users.noreply.github.com>
2020-10-27 12:38:27 +0700

    Update bayar.py
    List harga

--- userbot/modules/bayar.py
+++ userbot/modules/bayar.py
@@ -8,3 +8,13 @@ async def typewriter(typew):
     sleep(3)
     await typew.edit("📱DANA 085877240759 📱PULSA 082114264663")
 # Create by myself @localheart
+
+@register(outgoing=True, pattern='^.list(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(3)
+    await typew.edit("-  SSH PREMIUM SINGAPORE  15 k
+-  SSH PREMIUM INDONESIA  10 k
+-  TEMBAK OMG READY  pm @adjieptra
+- VPS INDONESIA/SINGAPORE READY 150-200 k")
+# Create by myself @localheart
