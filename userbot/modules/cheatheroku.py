7abf9aca99a27870cc80499a0027970895132217
zankee <66905046+zankee@users.noreply.github.com>
2020-10-27 17:30:00 +0700

    Create cheatheroku.py
    Heroku unlimited

---
+++ userbot/modules/cheatheroku.py
@@ -0,0 +1,34 @@
+from time import sleep
+
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.cheat(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(3)
+    await typew.edit("`Mwahahaha...Heroku mu aku cheat ya..😆😆`")
+    sleep(3)
+    await typew.edit("`Proses....`")
+    sleep(1)
+    await typew.edit("**HEROKU ANDA UDAH UNLIMITED BOSS,😎😎**")
+# Create by myself @localheart
+
+
+@register(outgoing=True, pattern='^.unli(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(3)
+    await typew.edit("**SISA HEROKU ANDA = UNLIMITED😎😎**")
+    
+# Create by myself @localheart
+
+
+
+
+
+
+  
+
+
+
