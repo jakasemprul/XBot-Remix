bea9462df396934b278203bcc198e3a389796e47
zankee <66905046+zankee@users.noreply.github.com>
2020-10-28 07:45:08 +0700

    Update kuliah.py
    DIS Comment

--- userbot/modules/kuliah.py
+++ userbot/modules/kuliah.py
@@ -76,6 +76,29 @@ async def _(event):
             await event.client.delete_messages(httpheader, response.message.message)
 
 
+@register(outgoing=True, pattern=r"^\.dis(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    event.pattern_match.group(1)
+    chat = "@unsiq_bot"  # pylint:disable=E0602
+    DIS = f"DIS"  # pylint:disable=E0602
+    await event.edit("Processing....")
+    async with bot.conversation("@unsiq_bot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=458359369))
+            await conv.send_message(f'{DIS} {No}')
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("Unblock @ unsiq_bot dulu Goblok!!")
+            return
+        else:
+            await event.edit(f"{response.message.message}")
+            await event.client.delete_messages(httpheader, response.message.message)
+
 CMD_HELP.update({
     "kuliah":
     "`.matkul`\
@@ -83,5 +106,7 @@ CMD_HELP.update({
 \n\n`.passwd`\
 \nUsage: untuk mengganti password e-learning.\
 \n\n`.idtele`\
-\nUsage: untuk cek id telegram ."
+\nUsage: untuk cek id telegram .\
+\n\n`.dis`\
+\nUsage: untuk komentar DIS (NOMER) ."
 })
