5081fb6206c4398844a0eb4d3bb8b7de94b3ddf8
zankee <66905046+zankee@users.noreply.github.com>
2020-10-29 08:47:53 +0700

    Update resi.py

--- userbot/modules/resi.py
+++ userbot/modules/resi.py
@@ -10,19 +10,19 @@ async def _(event):
     if event.fwd_from:
         return
     event.pattern_match.group(1)
-    chat = "@berlatihbot"  # pylint:disable=E0602
+    chat = "@GeDebugBetaBot"  # pylint:disable=E0602
     resi = f"resi"  # pylint:disable=E0602
     await event.edit("Processing....")
-    async with bot.conversation("@berlatihbot") as conv:
+    async with bot.conversation("@GeDebugBetaBot") as conv:
         try:
             response = conv.wait_event(
                 events.NewMessage(
                     incoming=True,
-                    from_users=-1001134152012))
+                    from_users=443213072))
             await conv.send_message(f'{kurir} {resi}')
             response = await response
         except YouBlockedUserError:
-            await event.reply("Unblock @ berlatihbot dulu Goblok!!")
+            await event.reply("Unblock @ GeDebugBetaBot dulu Goblok!!")
             return
         else:
             await event.edit(f"{response.message.message}")
