1d3372676f9c2f59ab08725ddd1c933bbbec8ee3
zankee <66905046+zankee@users.noreply.github.com>
2020-11-05 16:59:02 +0700

    Update transform.py

--- userbot/modules/transform.py
+++ userbot/modules/transform.py
@@ -4,7 +4,7 @@
 import os
 import random
 import numpy as np
-from color import Color
+from colour import Color
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from PIL import Image, ImageOps, ImageDraw, ImageFont
